f160157dba3cd3d1e04e911867ac452ac81c671c
R0rt1z2 <rogerortizleal@gmail.com>
2020-07-01 00:40:03 +0200

    Aquaris_M8: Fix the SplashScreen not showing up.
    * Don't set default brightness value, this causes weird errors at the startup and doesn't shows up the TWRP Splash Screen.
    
    Test: Flash the image without the brightness flag.
    Result: Succeed - SplashScreen shows up now.
    
    Signed-off-by: R0rt1z2 <rogerortizleal@gmail.com>
    Change-Id: Ib895ee30282576f129842f82c920cad4b2af2582

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,6 @@ TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_INCLUDE_CRYPTO := true
 TW_CRYPTO_USE_SYSTEM_VOLD := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_DEFAULT_BRIGHTNESS := 128
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TW_EXCLUDE_ENCRYPTED_BACKUPS := false
 TW_NO_EXFAT := false
